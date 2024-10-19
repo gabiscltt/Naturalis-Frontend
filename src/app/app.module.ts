f9df70f8b059c5c37349cddd7d22516ffa7a6943
Alessandra <alessandra.sucolotti@gmail.com>
2024-10-18 23:53:26 -0300

    Adding topbar + homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ItemService } from './services/item-service.service';
 import { HttpClientModule } from '@angular/common/http';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
